ff1d4ebb71d1ffc8e204ce761b9a49f109146e60
Vignesh <holmesvinn@outlook.com>
2017-02-19 00:18:25 +0530

    Update server.js

--- server.js
+++ server.js
@@ -8,5 +8,5 @@ var app = express();
 app.use(bodyParser.urlencoded({extended:true}));
 app.use(bodyParser.json());
 app.use('/api',require('./routes/api'));
-app.listen(3000);
+app.listen(80);
 console.log("server started at 3000");
