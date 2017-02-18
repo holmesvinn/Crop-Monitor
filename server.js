f7aa6ae1f00971104f4e1fc77c4ec084c90f2db6
holmesvinn <holmesvinn@gmail.com>
2017-02-18 23:44:18 +0530

    azure love project

---
+++ server.js
@@ -0,0 +1,12 @@
+var express  = require('express');
+var mongoose = require('mongoose');
+var bodyParser = require('body-parser');
+
+mongoose.connect('mongodb://vision:vision@ds159497.mlab.com:59497/vision');
+
+var app = express();
+app.use(bodyParser.urlencoded({extended:true}));
+app.use(bodyParser.json());
+app.use('/api',require('./routes/api'));
+app.listen(3000);
+console.log("server started at 3000");
