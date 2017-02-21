83e45fa1823758b14c2a9502fe6b246ee7b99a75
Vignesh <holmesvinn@outlook.com>
2017-02-21 20:54:50 +0530

    Update app.js

--- app.js
+++ app.js
@@ -6,7 +6,9 @@ var cookieParser = require('cookie-parser');
 var bodyParser = require('body-parser');
 
 var index = require('./routes/index');
-var data = require('./routes/data');
+var technical = require('./routes/technical');
+var general = require('./routes/general');
+var online = require('./routes/online');
 
 var app = express();
 
@@ -23,8 +25,9 @@ app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', index);
-app.use('/data', data);
-
+app.use('/technical', technical);
+app.use('/general',general);
+app.use('/online',online);
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
   var err = new Error('Not Found');
